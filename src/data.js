6315f90413ebf2d78526f4862eedcd8dd0fe4fb7
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2022-07-25 17:48:12 -0400

    add more data

--- src/data.js
+++ src/data.js
@@ -16,6 +16,24 @@ export default [
         endDate: "8 Jun, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
         imageUrl: "https://images.unsplash.com/photo-1523059623039-a9ed027e7fad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1432&q=80"
+    },
+    {
+        title: "New York City",
+        location: "New York",
+        googleMapsUrl: "https://goo.gl/maps/b18QSPbNcg3YG5PX9",
+        startDate: "08 Aug, 2013",
+        endDate: "15 Jun, 2013",
+        description: "New York City is one of the premire cities in the world and is a cultural landmark. It's known for its entertainment, its people, and it's pizza. There's nothing you can't do here",
+        imageUrl: "https://images.unsplash.com/photo-1617688319108-cb3bdc88f587?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80"
+    },
+    {
+        title: "Powell's City of Books",
+        location: "Portland",
+        googleMapsUrl: "https://g.page/powells-city-of-books?share",
+        startDate: "20 Jan, 2013",
+        endDate: "20 Jan, 2013",
+        description: "Iconic bookseller offering a diverse selection of used & new volumes, plus gifts for bibliophiles",
+        imageUrl: "https://images.unsplash.com/photo-1625878519938-5fdee26bb23a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
     }
 ]
 
