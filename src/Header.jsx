ce255e72dc045d1dae002448cb24b01d769e701c
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2022-07-25 06:12:32 -0400

    add header and styles

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <img src={'./globe-sm.png'} />
+            <h1 className='main-heading'>my travel journal</h1>
+        </header>
+    )
+}
