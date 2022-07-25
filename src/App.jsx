91f78573d5b1ca8daad4726657a89c8e06be56d6
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2022-07-25 15:44:52 -0400

    unitlize card containers, but styles broken

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
+import React from 'react'
 
 import './App.css'
 import Header from './Header'
+import Card from './Card'
+import data from './data'
 
 function App() {
+  const trips = data.map((trip) => {
+    return (
+      <Card 
+        data={trip}
+      />
+    )
+  })
   return (
     <div className="App">
       <Header />
-      <h1>greetings from app</h1>
-        
+      <div className="container">
+        <main>
+          {trips}
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Card(props) {
+    return (
+        <section className='card'>
+            <img src={props.data.imageUrl} className='card__img' />
+            <div className="card__info">
+                <div className="card__location">
+                    <h4>{props.data.location}</h4>
+                    <a href={props.data.googleMapsUrl} target="_blank">View on Google Maps</a>
+                </div>
+                <h3 className="card__title">{props.data.title}</h3>
+                <p className="card__date">{props.data.startDate} - {props.data.endDate}</p>
+                <p className="card__description">{props.data.description}</p>
+                
+            </div>
+        </section>
+    )
+}
